42526947dcb14dfadd20e06e4ef92bb8fe5725d8
My Name <cruhai@gmail.com>
2026-01-19 23:35:58 +0800

    Refactor Prisma to use singleton pattern

--- app/api/progress/route.js
+++ app/api/progress/route.js
@@ -1,7 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
+import prisma from "@/lib/prisma";
 
 export async function GET(req) {
     try {
